575453998eea87d15570922c685794d1a0fdcda9
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2024-01-10 13:02:13 +0100

    Update the example script such that it works with other cities as well

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import sys
 from weather_api import get_current_temperature
 
 def main():
-    city = "Brussels"
+    city = sys.argv[1]
     try:
         temperature = get_current_temperature(city)
         print(f"The current temperature in {city} is: {temperature}°C")
--- app/weather_api.py
+++ app/weather_api.py
@@ -1,14 +1,23 @@
 import os
+import random
 import requests
 from config import BASE_URL
 
 def get_current_temperature(city):
     """ Fetches the current temperature for a given city. """
 
+    API_KEY = os.environ.get('WEATHER_API_KEY')
+
+    # If the key did not exist, we will return a random temperature value (that can be used to test this script)
+    if not API_KEY:
+        print("No API key found, returning random temperature value.")
+        return random.randint(-10, 40)
+
     params = {
-        "key": os.environ['WEATHER_API_KEY'],
+        "key": API_KEY,
         "q": city
     }
+
     response = requests.get(BASE_URL, params=params)
     data = response.json()
     return data['current']['temp_c']
