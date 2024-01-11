f0c5abcc05d22f89ff2c8c2fd6d40f66ccbf4ddd
Pieter Verschaffelt <pieter.verschaffelt@ugent.be>
2024-01-11 10:28:09 +0100

    Add example documentation to the tool

--- app/weather_api.py
+++ app/weather_api.py
@@ -4,7 +4,19 @@ import requests
 from config import BASE_URL
 
 def get_current_temperature(city):
-    """ Fetches the current temperature for a given city. """
+    """ 
+    Fetches the current temperature for a given city from the WeatherAPI service. 
+    
+    WeatherAPI requires a valid API key in order to properly function. This key will be read from an environment
+    variable with the name WEATHER_API_KEY. If this variable does not exist, a random temperature value will be returned
+    instead.
+
+    Parameters:
+        city (str): The city to fetch the temperature for.
+
+    Returns:
+        A number representing the current temperature in degrees Celsius.
+    """
 
     API_KEY = os.environ.get('WEATHER_API_KEY')
 
